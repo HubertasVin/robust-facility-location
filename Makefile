8548037d1157da051c712a8b38f4904e7a699c84
HubertasVin <hubertas.personal@proton.me>
2026-05-03 00:12:57 +0300

    Makefile update.

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 build_run:
 	go run .
+
+visualise:
+	./venv/bin/python visualise_pareto.py checked_solutions.tsv pareto_visualisation.png
--- run_viz.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/hubertas/Documents/backup-folder/Programming/Go/robust-facility-location
-./venv/bin/python visualise_pareto.py checked_solutions.tsv pareto_visualisation.png
